2d0e67b7fa8b017c1c2bd9476170d7b120b35d8b
bo <1263462727@qq..com>
2020-06-26 11:40:39 +0800

    ..

---
+++ BFrameClient/Assets/Scripts/Test/TestLoadAssets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoadAssets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string pathAB = Application.streamingAssetsPath + "/Assets/Res/lua";
+        AssetBundle asset = AssetBundle.LoadFromFile(pathAB);
+
+        //Instantiate(asset.LoadAsset<GameObject>("CanvasA"));
+        //Instantiate(asset.LoadAsset<GameObject>("CanvasB"));
+        Debug.Log(asset.LoadAsset<TextAsset>("Main").text);
+        Debug.Log(asset.LoadAsset<TextAsset>("UIBase").text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
